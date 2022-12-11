d76ea61a8b47e5651e18113deeca0584d132c581
mmulvogue <mmulvogue@gmail.com>
2022-12-12 00:13:07 +1100

    Clean-up error handler

--- MM.WeatherService.Api/Controllers/ErrorController.cs
+++ MM.WeatherService.Api/Controllers/ErrorController.cs
@@ -1,40 +1,42 @@
-﻿using Microsoft.AspNetCore.Diagnostics;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MM.WeatherService.Api.Controllers;
 
-[ApiController]
+[AllowAnonymous]
+[ApiExplorerSettings(IgnoreApi = true)]
 public class ErrorController : ControllerBase
 {
     /// <summary>
-    ///     Returns default problem details for an application exception
+    ///     Returns default problem details response for an application exception
     /// </summary>
     /// <returns></returns>
-    [Route("/error")]
-    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("error")]
     public IActionResult HandleError()
     {
         return Problem();
     }
 
     /// <summary>
-    ///     Returns a problem details response with detailed exception info for development purposes
+    ///     Returns a problem details response with detailed exception info in title and message
     /// </summary>
     /// <param name="hostEnvironment"></param>
     /// <returns></returns>
-    [Route("/error-detailed")]
-    [ApiExplorerSettings(IgnoreApi = true)]
+    [Route("error-detailed")]
     public IActionResult HandleErrorWithDetails(
         [FromServices] IHostEnvironment hostEnvironment
     )
     {
+        // Short circuit in case used in prod environment to avoid leaking
+        // any implementation detail/private info from stack trace/error message
         if (!hostEnvironment.IsDevelopment()) return Problem();
 
-        var exceptionHandlerFeature =
-            HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+        var exceptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
 
         return Problem(
             exceptionHandlerFeature.Error.StackTrace,
-            title: exceptionHandlerFeature.Error.Message);
+            title: exceptionHandlerFeature.Error.Message
+        );
     }
 }
