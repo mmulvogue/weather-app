441562da253b0b68fdb373cbd58bb401bc8f6d73
mmulvogue <mmulvogue@gmail.com>
2022-12-12 00:12:37 +1100

    Add unit test for ApiKeyService
    - Separate the ApiKeyStore to allow mocking in test

--- MM.WeatherService.Api/ApiKeyAuth/ApiKeyService.cs
+++ MM.WeatherService.Api/ApiKeyAuth/ApiKeyService.cs
@@ -7,18 +7,24 @@ public interface IApiKeyService
 
 public class ApiKeyService : IApiKeyService
 {
-    private readonly string[] _apiKeyStore =
-    {
-        "API-dummy.key.1",
-        "API-dummy.key.2",
-        "API-dummy.key.3",
-        "API-dummy.key.4",
-        "API-dummy.key.5"
-    };
+    private readonly IApiKeyStore _apiKeyStore;
 
-    public Task<bool> IsValidKeyAsync(string apiKey)
+    public ApiKeyService(IApiKeyStore apiKeyStore)
     {
-        var result = _apiKeyStore.Contains(apiKey);
-        return Task.FromResult(result);
+        _apiKeyStore = apiKeyStore;
+    }
+
+    /// <summary>
+    ///     Checks if provided key is a valid api key
+    /// </summary>
+    /// <param name="apiKey">key to validate</param>
+    /// <returns>true/false indicating if api key is valid or not</returns>
+    public async Task<bool> IsValidKeyAsync(string apiKey)
+    {
+        // Simple shape validation check to avoid a data store lookup
+        if (!apiKey.StartsWith("API-")) return false;
+
+        // Finally check if key exists in the data store
+        return await _apiKeyStore.ExistsAsync(apiKey);
     }
 }
